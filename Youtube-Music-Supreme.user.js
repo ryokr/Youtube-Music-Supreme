64062ac7fb4d425ee4d513091008520e68aa817b
RyoKr <115417859+ryokr@users.noreply.github.com>
2026-07-07 13:47:10 +0700

    Add Youtube Music Supreme userscript

---
+++ Youtube-Music-Supreme.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Youtube Music Supreme
+// @namespace    https://github.com/ryokr/Youtube-Music-Supreme
+// @version      4.0.0
+// @description  Youtube Music Enhanced
+// @author       ryokr
+// @match        https://www.youtube.com/*
+// @match        https://music.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=music.youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(() => {window._lact = Date.now()}, 900000);
+
+})();
